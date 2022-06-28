172340efe02789fe7cf6d9d14cfe214bfea39a12
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-28 15:29:32 -0500

    fix gear undefined error

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,40 +1,42 @@
-import { Profile } from '../models/profile.js'
+import { Profile } from "../models/profile.js";
+import { Gear } from "../models/gear.js";
 
 function index(req, res) {
   let modelQuery = req.query.name
-    ? { name: new RegExp(req.query.name, 'i') }
-    : {}
+    ? { name: new RegExp(req.query.name, "i") }
+    : {};
   Profile.find(modelQuery)
-  .sort("name")
-  .then(profiles => {
-    res.render("profiles/index", { 
-      title: 'Profiles',
-      profiles: profiles, 
-      name: req.query.name,
+    .sort("name")
+    .then((profiles) => {
+      res.render("profiles/index", {
+        title: "Profiles",
+        profiles: profiles,
+        name: req.query.name,
+      });
     })
-  })
-  .catch(err => {
-    if (err) return next(err)
-  })
+    .catch((err) => {
+      if (err) return next(err);
+    });
 }
 
 function show(req, res) {
   Profile.findById(req.params.id)
-  .then(profile => {
-    const isSelf = profile._id.equals(req.user.profile._id)
-    res.render("profiles/show", {
-      title: `${profile.name}'s profile`,
-      profile,
-      isSelf
+    .then((profile) => {
+      const isSelf = profile._id?.equals(req.user.profile._id);
+      Gear.find({})
+      .then((gear) => {
+        res.render("profiles/show", {
+          title: `${profile.name}'s profile`,
+          gear,
+          profile,
+          isSelf,
+        });
+      });
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/')
-  })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
 }
 
-export {
-  index,
-  show
-}
+export { index, show };
--- models/profile.js
+++ models/profile.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  gear: {type: Schema.Types.ObjectId, ref: "Gear"},
   comments: {type: Schema.Types.ObjectId, ref: "Comment"}
 }, {
   timestamps: true
